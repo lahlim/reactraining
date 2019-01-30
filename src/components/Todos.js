af7659c2031ee1eebad5bbc37245a68c95a29446
lahlim <lassi_mustonen@hotmail.com>
2019-01-30 17:17:50 +0200

    Edetty tutoriaalissa vaiheeseen, missä voi chekata todoitemin

---
+++ src/components/Todos.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Todoitem from './Todoitem';
+import PropTypes from 'prop-types';
+
+export class Todos extends Component {
+    render() {
+    return this.props.todos.map((todo) => (
+        
+        <Todoitem key={todo.id} todo={todo} markComplete=
+        {this.props.markComplete} />
+    ));
+  }
+}
+
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
+}
+
+export default Todos;
